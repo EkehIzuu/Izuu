29d04bcc8f7d4fc68d135a63c4b3965b3072590a
EkehIzuu <139346551+EkehIzuu@users.noreply.github.com>
2023-07-17 09:11:26 +0100

    Add files via upload

---
+++ defence/assets/defence.js
@@ -0,0 +1,48 @@
+// alert('hey guys!')
+
+// let welcome = prompt('hey guy wetin come be your name?')
+
+// alert(confirm('welcome' + welcome + 'moroski')
+
+// let myValue = document.querySelector('#myValue')
+// let btn = document.querySelector('#BtnGet')
+
+
+// btnGet.addEventListener ('click', (e) => {
+//     if (myValue.trim() == '') {
+//     alert("Please enter value to get result");}
+//     else{
+//         e.preventDefault()
+//         EventTarget.textContent = myValue.Value
+//     }
+// })
+// function calculateArea(radius)
+// let area = Ekeh(radius2)
+// return area
+
+let num1 = document.getElementById('num1')
+let num2 = document.getElementById('num2')
+let btnAdd = document.getElementById('btnAdd')
+let displayValue = document.getElementById('displayValue')
+
+function addNum() {
+    let sum = +num1.value + +num2.value
+    return displayValue.innerHTML = sum
+}
+//Aray in JS
+
+// let names = 'mart'
+
+// const myArray = [true, 'izuu', 55, [5, 'Ekeh'], {age: 65, phone: '5555555555555'}]
+// console.log('13/06/2023')
+// console.log(myArray[3][1])
+
+// console.log(myArray[myArray.length -1][age])
+
+// Array methods
+
+// console.log(myArray)
+// myArray.push('garri')
+// myArray.pop('garri')
+// myArray.shift('garri')
+// myArray.unshift('garri')
